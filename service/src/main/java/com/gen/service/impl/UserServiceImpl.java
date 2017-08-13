5e4f09663ebb0c2eff7dad98f9925c5cd5bff2f8
wangjingyu <272663856@qq.com>
2017-08-13 23:08:49 +0800

    0813提交

--- service/src/main/java/com/gen/service/impl/UserServiceImpl.java
+++ service/src/main/java/com/gen/service/impl/UserServiceImpl.java
@@ -1,10 +1,13 @@
 package com.gen.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.gen.entity.User;
 import com.gen.mapper.UserMapper;
 import com.gen.service.UserService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +18,8 @@ import java.util.List;
  */
 @Service
 public class UserServiceImpl implements UserService{
+
+    private final static Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
     @Autowired
     private UserMapper userMapper;
     @Override
@@ -23,6 +28,7 @@ public class UserServiceImpl implements UserService{
         //用PageInfo对结果进行包装
         List<User> userList = userMapper.findList();
         PageInfo page = new PageInfo(userList);
+        logger.info("查询结果 service={}",new Object[]{JSON.toJSONString(page)});
         return page;
     }
 }
--- web/src/main/com/gen/controller/UserController.java
+++ web/src/main/com/gen/controller/UserController.java
@@ -4,6 +4,8 @@ import com.alibaba.fastjson.JSON;
 import com.gen.entity.User;
 import com.gen.service.UserService;
 import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    private final static Logger logger = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private UserService userService;
 
@@ -21,7 +24,7 @@ public class UserController {
     public String toList(HttpServletRequest request, Model model) {
         PageInfo<User> pageInfo = userService.findList();
         model.addAttribute("pageInfo", pageInfo);
-        System.out.println(JSON.toJSONString(pageInfo));
+        logger.info("查询结果={}",new Object[]{JSON.toJSONString(pageInfo)});
         return "user/list";
     }
 }
